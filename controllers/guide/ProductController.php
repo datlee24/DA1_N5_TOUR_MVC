47d882d23fe480337d367be3dcd83c7534d1cccd
datlee24 <ldat24062006@gmail.com>
2025-11-29 14:34:59 +0700

    điều chỉnh lại đường dẫn giao diện

--- controllers/guide/ProductController.php
+++ controllers/guide/ProductController.php
@@ -11,9 +11,7 @@ class ProductController
 
     public function Home()
     {
-        $title = "Đây là trang chủ nhé hahaa";
-        $thoiTiet = "Hôm nay trời có vẻ là mưa";
-        require_once './views/trangchu.php';
+        require_once PATH_GUIDE . "trangchu.php";
     }
     
 }
