eba9654afa5ba64b6b3890bc5f48e3726fa1ce35
baolong <email@example.com>
2025-12-01 22:58:21 +0700

    create controller customer

---
+++ controllers/guide/GuideCustomerController.php
@@ -0,0 +1,29 @@
+<?php
+
+class GuideCustomerController
+{
+    protected $customerModel;
+
+    public function __construct()
+    {
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
+        checkIsGuide();
+
+        $this->customerModel = new CustomerModel();
+    }
+
+    /**
+     * Danh sách khách hàng (dùng bảng customer)
+     */
+    public function index()
+    {
+        $customers = $this->customerModel->getAll();
+
+        require_once PATH_GUIDE . "customers.php";
+    }
+}
+
+
